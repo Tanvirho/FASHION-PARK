1f71d305cf3ffcfa075188bc69a7d5525cc969c2
Anonymous20058 <tanvirhossien13@gmail.com>
2025-11-24 20:09:44 +0600

    commit

---
+++ src/components/Shop.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Header from './header'
+import Popular from './popular'
+import Offer from './Offer'
+import Collections from './Collections'
+import Newsletter from './Newletter'
+
+
+function Shop() {
+    return (
+        <div>
+            <Header />
+            <Popular />
+            <Offer />
+            <Collections />
+            <Newsletter />
+
+        </div>
+    )
+}
+
+export default Shop
