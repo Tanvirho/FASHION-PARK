5bf84a5c50fe24c799745e34ba0b449b2c64d229
Tanvir Hossain Shifat <74765686+Tanvirho@users.noreply.github.com>
2025-11-24 20:29:47 +0600

    Update Shop.jsx

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Header from './header'
+import Header from './Header'
 import Popular from './popular'
 import Offer from './Offer'
 import Collections from './Collections'
